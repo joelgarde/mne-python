affb8fc16a80c8144d7aad232139a77007c68a0a
Joel Garde <jgarde@enst.fr>
2022-08-31 10:47:32 +0200

    changing residual computation

--- mne/dipole_forward.py
+++ mne/dipole_forward.py
@@ -5,7 +5,7 @@ from mne.transforms import apply_trans
 
 import numpy as np
 import scipy.sparse as sp
-
+from utils.experiment import memory
 from mne.dipole import _make_guesses
 
 from .bem import _bem_find_surface
@@ -72,6 +72,7 @@ def K_EEG_MEG(r, r_bem, SMEG, SEEG, coils:Coils):
     K_MEG = (K_inf + KMEG) * _MAG_FACTOR
     return np.r_[KEEG, KMEG]
 
+
 def G_infty(n, r): 
     """compute the K_infty matrix (eq 40 of 10.1109/10.748978), the infinity potential design matrix.
     return:
@@ -83,9 +84,53 @@ def G_infty(n, r):
     G = np.einsum("ij,ijk->ijk", d ** (-3/2), D)
     return G.reshape((n.shape[0], -1))
 
+def prepare_forward(info, bem, cov, trans, ch_types, guess_grid=0.02):
+    picks = pick_types(info, meg=True, eeg=True, ref_meg=False)
 
-def grad_(r, r_bem, SMEG, SEEG, coils:Coils, whitener):
-    ...
+    mri_head_t, _  = _get_trans(trans)
+    head_mri_t, _ = _get_trans(trans, fro='head', to='mri')
+    neeg = len(pick_types(info, meg=False, eeg=True, ref_meg=False,
+                        exclude=[])) 
+    bem = _setup_bem(bem, f"{bem}", neeg, mri_head_t, verbose=False)
+    cov = _ensure_cov(cov)
+    whitener, _, rank = compute_whitener(cov, info, picks=picks,
+                                        rank=None, return_rank=True)
+
+    ### perform sanity checks.
+    if _needs_eeg_average_ref_proj(info):
+        raise ValueError('EEG average reference is mandatory for dipole '
+                        'fitting.')
+
+    if bem['is_sphere']:
+        raise NotImplementedError('method only implemented for mesh bem.')
+
+    # Forward model setup
+    megcoils, compcoils, megnames, meg_info = [], [], [], None
+    eegels, eegnames = [], []
+    if 'grad' in ch_types or 'mag' in ch_types:
+        megcoils, compcoils, megnames, meg_info = \
+            _prep_meg_channels(info, exclude='bads',
+                            accuracy="normal", verbose=False)
+    if 'eeg' in ch_types:
+        eegels, eegnames = _prep_eeg_channels(info, exclude='bads',
+                                            verbose=False)
+
+    fwd_data = dict(coils_list=[megcoils, eegels], infos=[meg_info, None],
+                ccoils_list=[compcoils, None], coil_types=['meg', 'eeg'],)
+
+    _prep_field_computation(None, bem, fwd_data, n_jobs=1,
+                        verbose=False)
+
+    # initial grid search space.
+    min_dist_to_inner_skull = 5.  / 1000.
+    guess_mindist = max(0.005, min_dist_to_inner_skull)
+    guess_exclude = 0.02
+    inner_skull = _bem_find_surface(bem, 'inner_skull')
+    guess_src = _make_guesses(inner_skull, guess_grid, guess_exclude,
+                                guess_mindist, n_jobs=1)[0]
+    
+    return guess_src, fwd_data, picks, megcoils, whitener, head_mri_t
+
 
 def fit_dipole(evoked, cov, bem, trans, verbose=None):
     """
@@ -96,53 +141,15 @@ def fit_dipole(evoked, cov, bem, trans, verbose=None):
     info = evoked.info
     times = evoked.times.copy()
     comment = evoked.comment
-    picks = pick_types(info, meg=True, eeg=True, ref_meg=False)
+    
+    
+    (guess_src, 
+    fwd_data, 
+    picks, 
+    megcoils, 
+    whitener, 
+    head_mri_t) = prepare_forward(info, bem, cov, trans, evoked.get_channel_types(), guess_grid=0.005)
     data = evoked.data[picks]
-
-    mri_head_t, _  = _get_trans(trans)
-    head_mri_t, _ = _get_trans(trans, fro='head', to='mri')
-    neeg = len(pick_types(info, meg=False, eeg=True, ref_meg=False,
-                          exclude=[])) 
-    bem = _setup_bem(bem, f"{bem}", neeg, mri_head_t, verbose=False)
-    cov = _ensure_cov(cov)
-    whitener, _, rank = compute_whitener(cov, info, picks=picks,
-                                         rank=None, return_rank=True)
-
-    ### perform sanity checks.
-    if _needs_eeg_average_ref_proj(evoked.info):
-        raise ValueError('EEG average reference is mandatory for dipole '
-                         'fitting.')
-    if not np.isfinite(data).all():
-        raise ValueError('Evoked data must be finite')
-    if bem['is_sphere']:
-        raise NotImplementedError('method only implemented for mesh bem.')
-
-    # Forward model setup
-    megcoils, compcoils, megnames, meg_info = [], [], [], None
-    eegels, eegnames = [], []
-    ch_types = evoked.get_channel_types()
-    if 'grad' in ch_types or 'mag' in ch_types:
-        megcoils, compcoils, megnames, meg_info = \
-            _prep_meg_channels(info, exclude='bads',
-                               accuracy="normal", verbose=verbose)
-    if 'eeg' in ch_types:
-        eegels, eegnames = _prep_eeg_channels(info, exclude='bads',
-                                              verbose=verbose)
-
-    fwd_data = dict(coils_list=[megcoils, eegels], infos=[meg_info, None],
-                ccoils_list=[compcoils, None], coil_types=['meg', 'eeg'],)
-
-    _prep_field_computation(None, bem, fwd_data, n_jobs=1,
-                        verbose=False)
-
-    # initial grid search space.
-    min_dist_to_inner_skull = 5.  / 1000.
-    guess_grid = 0.02
-    guess_mindist = max(0.005, min_dist_to_inner_skull)
-    guess_exclude = 0.02
-    inner_skull = _bem_find_surface(bem, 'inner_skull')
-    guess_src = _make_guesses(inner_skull, guess_grid, guess_exclude,
-                                guess_mindist, n_jobs=1)[0]
 
     
     # mapping to my data:
@@ -151,6 +158,7 @@ def fit_dipole(evoked, cov, bem, trans, verbose=None):
     SEEG = fwd_data['solutions'][1] or np.empty((0,rr_bem.shape[0])) 
     SMEG = fwd_data['solutions'][0]
     B = whitener @ data
+    B2 = np.einsum("ij,ij->j", B, B)
     coils = Coils.from_mne_coils(megcoils)
     n_mri = apply_trans(head_mri_t, coils.n)
     r_mri = apply_trans(head_mri_t, coils.r)
@@ -158,28 +166,40 @@ def fit_dipole(evoked, cov, bem, trans, verbose=None):
 
 
     # fit dipoles!
+
     K: Never = whitener @ K_EEG_MEG(rr, rr_bem, SMEG, SEEG, coils)
     K = K.reshape((-1, K.shape[-1]//3, 3))
     K = np.swapaxes(K, 0, 1) # batch per dipole.
-    U,S,VT = np.linalg.svd(K, full_matrices=False)
-    residuals = np.zeros((rr.shape[0],2, B.shape[1]))
-    reg_mask = S[:,0] <  5* S[:,2]
-    residuals[reg_mask] = U[reg_mask, :, :2].swapaxes(1,2) @ B #residuals due to regularisation. Please see: 10.1088/0031-9155/32/1/004 (last §)
-    Bm2  = np.einsum('ijk,ijk->ik', residuals, residuals)
-    B2 = np.einsum('ij,ij->j', B,B)
-    gof = (Bm2 / B2)
+    U,S,VT = np.linalg.svd(K, full_matrices=False)    
+    #U: (n_dipole, n_sensor, 3).
+    deflate_mask = S[..., 0] > 5 * S[..., 2]
+    U[deflate_mask, :, 2] = 0
+    projection = np.swapaxes(U, 1, 2) @ B
+    Bm2 = np.einsum("ijk,ijk->ik", projection, projection)
+    r0i, t0i = np.unravel_index(np.argmax(Bm2), Bm2.shape)
+    r0 = rr[r0i]
 
-    r0idx = np.argmax(gof[:,1])
-    r0 = rr[r0idx]
-
+    fits = []
     def fit_fn(r):
         K = whitener @ K_EEG_MEG(r, rr_bem, SMEG, SEEG, coils)
-        U,S,V = np.linalg.svd(K)
-        reg_mask = S[0] <  5* S[2]
-        residuals = np.zeros(B.shape[1])
-        residuals[reg_mask] =  U[:, 2] @ B
-        return np.max(residuals**2)
+        U,S,V = np.linalg.svd(K, full_matrices=False)
+        deflate_mask = S[..., 0] > 5 * S[..., 2]
+        if np.any(deflate_mask):
+            print(f"regularisation applied ! Ratio:  {S[0]/S[2]: .2f}")
+        U = np.expand_dims(U, 0)
+        U[deflate_mask, : , 2] = 0
+        projection = np.swapaxes(U, 1, 2) @ B
+        Bm2 = np.einsum("ijk,ijk->ik", projection, projection) # projected  norm2 per dipole per time.
+        fit = 1 - (Bm2[0,t0i] / B2[t0i])
+        fits.append(fit)
+        return fit
 
-    rhat = minimize(fit_fn, r0, method='Nelder-Mead', options=dict(disp=True), callback=print)
-    
-    return r0, rhat
+    path = [r0]
+    def record(r):
+        path.append(r)
+
+    rhat = minimize(fit_fn, r0, method='Nelder-Mead', options=dict(disp=True), callback=record, tol=1e-8)
+
+    fits = np.asarray(fits)
+    path = np.asarray(path)
+    return rhat, path, fits
